952653e5a8d2ebe82a718ac029b34a2b9880c7f0
tata1810 <agathaangel1826@gmail.com>
2024-11-16 17:33:00 +0700

    edit home.py

--- home.py
+++ home.py
@@ -1,23 +1,22 @@
 import streamlit as st
-
 from streamlit_extras.switch_page_button import switch_page
 
 
 st.set_page_config(
-    page_title='HOME',
-    initial_sidebar_state='collapsed'
+    page_title='Home',
+    page_icon='🏠',
+    initial_sidebar_state='collapsed',
+    layout='centered'
 )
 
 for key in st.session_state.keys():
     del st.session_state[key]
 
 st.markdown("""
-            # :wave:  **Wilujeung Sumping di Pembelajaran Aksara Sunda**
-            
+            # 👋🏻 **Wilujeung Sumping di Pembelajaran Aksara Sunda**
             ##### Silahkan sign up atau log in jika sudah memiliki akun
             ---
             """)
-
 
 signup, login = st.columns(2)
 if signup.button(label='Belum punya akun', type='primary', use_container_width=True):
